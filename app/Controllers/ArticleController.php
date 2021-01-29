4742e5044b3e9e389642c8364dd9c5696308c824
Ferdiansyah <ferdif9996@gmail.com>
2021-01-29 18:33:45 +0700

    Add validation ArticleController.php

--- app/Controllers/ArticleController.php
+++ app/Controllers/ArticleController.php
@@ -55,53 +55,73 @@ class ArticleController extends ResourceController
     }
     public function show($id = null)
     {
-        $data = $this->model->select('app_article.*, app_user.name, app_user.avatar')
+        $data = $this->model->select('app_article.*, app_user.name, app_user.avatar, app_user.bio')
         ->join('app_user', 'app_article.user_id = app_user.id')
         ->where('app_article.id', $id)->get()->getRow();
         return $this->respond($data);
     }
     public function create()
     {
-        // $validation =  \Config\Services::validation();
-        // $validation->setRules([
-        //     'category_id' => 'required',
-        //     'password' => 'required|min_length[10]'
-        // ]);
-        $check = $this->protect->check($this->request->getServer('HTTP_AUTHORIZATION'));
-        if(!empty($check->{'message'}) && $check->message == 'Access Granted'){
-            $files = $this->request->getFile('image');
-            $files->move(WRITEPATH.'uploads/'. $check->data->id);
-        	$this->model->insert_data([
-                'user_id' => $check->data->id,
-                'category_id' => $this->request->getPost('category_id'),
-                'title' => $this->request->getPost('title'),
-                'content' => $this->request->getPost('content'),
-                'description' => $this->request->getPost('description'),
-                'image' => $files->getName(),
-                'status' => $this->request->getPost('status'),
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s')
-            ]);
-            return $this->respond(['message' => 'Successfuly add data']);
+        $validation =  \Config\Services::validation();
+        $validation->setRules([
+            'category_id' => 'required',
+            'title' => 'required|min_length[5]|max_length[100]',
+            'description' => 'required|max_length[255]',
+            'content' => 'required|min_length[5]',
+            'image' => 'uploaded[image]|max_size[image,5120]'
+        ]);
+        if($validation->withRequest($this->request)->run() === true)
+        {
+            $check = $this->protect->check($this->request->getServer('HTTP_AUTHORIZATION'));
+            if(!empty($check->{'message'}) && $check->message == 'Access Granted'){
+                $files = $this->request->getFile('image');
+                $files->move(WRITEPATH.'uploads/'. $check->data->id);
+            	$this->model->insert_data([
+                    'user_id' => $check->data->id,
+                    'category_id' => $this->request->getPost('category_id'),
+                    'title' => $this->request->getPost('title'),
+                    'content' => $this->request->getPost('content'),
+                    'description' => $this->request->getPost('description'),
+                    'image' => $files->getName(),
+                    'status' => $this->request->getPost('status'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s')
+                ]);
+                return $this->respond(['message' => 'Successfuly add data']);
+            }else{
+                return $this->respond(['message' => 'Access Denied'], 401);
+            }
         }else{
-            return $this->respond(['message' => 'Access Denied'], 401);
+            return $this->respond(['message' => $validation->listErrors()], 403);
         }
     }
     public function update($id = null)
     {
-        $check = $this->protect->check($this->request->getServer('HTTP_AUTHORIZATION'));
-        if(!empty($check->{'message'}) && $check->message == 'Access Granted'){
-            $data = $this->request->getJSON(true);
-            if($this->request->getFile('image')) {
-                $files = $this->request->getFile('image');
-                $files->move(WRITEPATH.'uploads/'. $check->data->id);
-                $data['image'] = $files->getName();
+        $validation =  \Config\Services::validation();
+        $validation->setRules([
+            'category_id' => 'required',
+            'title' => 'required|min_length[5]|max_length[100]',
+            'description' => 'required|max_length[255]',
+            'content' => 'required|min_length[5]',
+        ]);
+        if($validation->withRequest($this->request)->run() === true)
+        {
+            $check = $this->protect->check($this->request->getServer('HTTP_AUTHORIZATION'));
+            if(!empty($check->{'message'}) && $check->message == 'Access Granted'){
+                $data = $this->request->getJSON(true);
+                if($this->request->getFile('image')) {
+                    $files = $this->request->getFile('image');
+                    $files->move(WRITEPATH.'uploads/'. $check->data->id);
+                    $data['image'] = $files->getName();
+                }
+                $data['updated_at'] = date('Y-m-d H:i:s');
+            	$this->model->update_data($data, $id);
+                return $this->respond(['message' => 'Successfuly update data']);
+            }else{
+                return $this->respond(['message' => 'Access Denied'], 401);
             }
-            $data['updated_at'] = date('Y-m-d H:i:s');
-        	$this->model->update_data($data, $id);
-            return $this->respond(['message' => 'Successfuly update data']);
         }else{
-            return $this->respond(['message' => 'Access Denied'], 401);
+            return $this->respond(['message' => $validation->listErrors()], 403);
         }
     }
     public function delete($id = null)

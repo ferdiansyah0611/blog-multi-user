d2d669c399bf710feb6fd2927af4a7ad3b5ffa73
Ferdiansyah <ferdif9996@gmail.com>
2021-02-02 03:43:32 +0700

    Cleaning code Loader.jsx

--- resources/js/component/tools/Loader.jsx
+++ resources/js/component/tools/Loader.jsx
@@ -3,20 +3,20 @@ import React from 'react';
 const Loader = (props) => {
 	return(
 		<div className="row">
-          <div className="col s12 center-align" style={{marginTop:130, paddingBottom: 130}}>
-            <div className="preloader-wrapper big active">
-              <div className="spinner-layer spinner-blue-only">
-                <div className="circle-clipper left">
-                  <div className="circle"></div>
-                </div><div className="gap-patch">
-                  <div className="circle"></div>
-                </div><div className="circle-clipper right">
-                  <div className="circle"></div>
-                </div>
-              </div>
+      <div className="col s12 center-align" style={{marginTop:130, paddingBottom: 130}}>
+        <div className="preloader-wrapper big active">
+          <div className="spinner-layer spinner-blue-only">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div><div className="gap-patch">
+              <div className="circle"></div>
+            </div><div className="circle-clipper right">
+              <div className="circle"></div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
 	)
 }
 export default Loader;

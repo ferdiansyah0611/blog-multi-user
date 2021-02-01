51eb23c3dbc17328240edfafce501862ec51f5a4
Ferdiansyah <ferdif9996@gmail.com>
2021-02-02 02:15:52 +0700

    Update BreadCrumb.jsx

--- resources/js/component/tools/BreadCrumb.jsx
+++ resources/js/component/tools/BreadCrumb.jsx
@@ -2,20 +2,30 @@ import React from 'react';
 import {
   Link,
 } from "react-router-dom";
+/*context*/
+import ContextDATA from '../../ContextDATA';
+
 const BreadCrumb = (props) => {
   return(
-    <nav className="light-blue darken-4 z-depth-0">
-      <div className="nav-wrapper">
-        <div className="col s12 ml-10px">
-          <Link to="/" className="breadcrumb">/</Link>
-        {
-          props.data.map((data, key) => {
-            return(<Link to={data.url} className="breadcrumb" key={key}>{data.str}</Link>)
-          })
-        }
-        </div>
-      </div>
-    </nav>
+    <ContextDATA.Consumer>
+      {
+        result => (
+          <nav className={"darken-4 z-depth-0 " + result.ui.navbar.bg}>
+            <div className="nav-wrapper">
+              <div className="col s12 ml-10px">
+                <Link to="/" className="breadcrumb">/</Link>
+                {
+                  props.data.map((data, key) => {
+                    return(<Link to={data.url} className="breadcrumb" key={key}>{data.str}</Link>)
+                  })
+                }
+              </div>
+            </div>
+          </nav>
+
+        )
+      }
+    </ContextDATA.Consumer>
   )
 }
 export default BreadCrumb;

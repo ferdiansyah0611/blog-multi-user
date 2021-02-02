1fb10896fac9affa24c05637db0584f10bed7d11
Ferdiansyah <ferdif9996@gmail.com>
2021-02-03 06:28:00 +0700

    Update Home.jsx

--- resources/js/component/Home.jsx
+++ resources/js/component/Home.jsx
@@ -13,26 +13,72 @@ import ContextDATA from '../ContextDATA';
 class Home extends React.Component{
   constructor(props){
     super(props)
+    this.state = {
+      users: []
+    }
   }
   componentDidMount(){
     document.title = 'Home Pages | Go Blog'
+    axios.get(BaseUrl + 'api/user?random=true').then(result => {
+      this.setState({users: result.data.data})
+    })
   }
   render(){
     return(
       <React.Fragment>
         <BreadCrumb data={[{url: '/', str: 'Home'}]} />
         <Slider url={`${BaseUrl}api/article`} query="popular=true"  />
-        <h5 className="ml-10px">Article</h5>
-        <ArticleLoadCMP url={BaseUrl + 'api/article'} id_next="next_article"/>
         <ContextDATA.Consumer>
           {
             result => (
-              result.users.id ?
-                <React.Fragment>
-                  <h5 className="ml-10px">My Subscribe</h5>
-                  <ArticleLoadCMP url={BaseUrl + 'api/article-subscribe'} id_next="next_article_subscribe" query="paginate=true" headers={true}/>
-                </React.Fragment>
-              : false
+              <React.Fragment>
+                <h5 className="ml-10px">Article</h5>
+                <ArticleLoadCMP url={BaseUrl + 'api/article'} id_next="next_article"/>
+                <div className="row">
+                  <div className="col s12 m6">
+                    <ul className="collection with-header">
+                      <li className="collection-header"><h4>Category</h4></li>
+                      {
+                        result.category.map((data, key) => {
+                          return(
+                            <li className="collection-item avatar" key={key}>
+                              <i className="material-icons circle">folder</i>
+                              <span className="title"><Link to={'/category/' + data.name}>{data.name}</Link></span>
+                              <p>{data.description}</p>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </div>
+                  <div className="col s12 m6">
+                    <ul className="collection with-header">
+                      <li className="collection-header">
+                        <h4>Recommended People</h4>
+                      </li>
+                      {
+                        this.state.users.map((data, key) => {
+                          return(
+                            <li className="collection-item avatar" key={key}>
+                              <img className="circle" src={BaseUrl + 'api/usrfile/' + data.id + '/' + data.avatar} />
+                              <span className="title"><Link to={'/profile/' + data.id}>{data.name}</Link></span>
+                              <p>{data.bio ? data.bio: 'Bio is not created by its users'}</p>
+                            </li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </div>
+                </div>
+                {
+                  result.users.id ?
+                    <React.Fragment>
+                      <h5 className="ml-10px">My Subscribe</h5>
+                        <ArticleLoadCMP url={BaseUrl + 'api/article-subscribe'} id_next="next_article_subscribe" query="paginate=true" headers={true}/>
+                      </React.Fragment>
+                  : false
+                }
+              </React.Fragment>
             )
           }
         </ContextDATA.Consumer>

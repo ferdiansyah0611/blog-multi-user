88c0dee1e0fe227c0d3cc3d4226d5af1433fc439
Ferdiansyah <ferdif9996@gmail.com>
2021-02-04 08:21:29 +0700

    Add Config.js

---
+++ resources/js/Config.js
@@ -0,0 +1,23 @@
+/**
+	Config for default value
+*/
+const Config = {
+	navbar: {
+		bg: 'blue darken-2',
+		txt: 'white-text'
+	},
+	users: {
+		cover: 'https://images.unsplash.com/photo-1606044466411-207a9a49711f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=300&q=60',
+		avatarDefault: 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80'
+	},
+	sidebar: {
+    	bg: '',
+    	txt: '',
+    	cover: 'https://images.unsplash.com/photo-1606044466411-207a9a49711f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=300&q=60'
+    },
+    footer: {
+    	bg: 'light-blue',
+    	status: true
+    }
+}
+export default Config

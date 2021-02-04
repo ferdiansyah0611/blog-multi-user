d3ff3258bd6ae5aedbfb39e1cf67588aeffc32b0
Ferdiansyah <ferdif9996@gmail.com>
2021-02-04 08:20:37 +0700

    Add UserFakerSeeder.php

---
+++ app/Database/Seeds/UserFakerSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class UserFakerSeeder extends Seeder
+{
+	public function run()
+	{
+		$faker = \Faker\Factory::create('id_ID');
+		for ($i = 0; $i < 1000; $i++) {
+			$jk = $faker->randomElement(['pria', 'wanita']);
+			if ($jk == "pria") {
+				$gender = "male";
+			} else {
+				$gender = "female";
+			}
+			$data = [
+				'id' => rand(),
+				'name' => $faker->name($gender),
+				'email' => $faker->email,
+				'password' => password_hash('12345678', PASSWORD_BCRYPT),
+				'born' => $faker->date('Y-m-d', 'now'),
+				'gender' => $jk,
+				'location' => $faker->address,
+				'role' => 'user',
+				'type' => '0',
+				'bio' => 'bio',
+				'avatar' => '',
+				'created_at' => date('Y-m-d H:i:s'),
+				'updated_at' => date('Y-m-d H:i:s')
+			];
+			$this->db->table('app_user')->insert($data);
+		}
+	}
+}

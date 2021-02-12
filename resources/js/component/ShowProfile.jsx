035c367c730b6eb29b04375faf08d735dfb1431c
Ferdiansyah <ferdif9996@gmail.com>
2021-02-13 06:19:17 +0700

    Add 2 card element ShowProfile.jsx

--- resources/js/component/ShowProfile.jsx
+++ resources/js/component/ShowProfile.jsx
@@ -25,6 +25,8 @@ class ShowProfileCMP extends React.Component {
     this.state = {
       users: {},
       statusSubscribe: 'Subscribe',
+      totalArticle: 0,
+      totalSubscribe: 0,
       finished: false,
       found: false
     }
@@ -67,6 +69,14 @@ class ShowProfileCMP extends React.Component {
             this.setState({statusSubscribe: 'Subscribed'})
           }
         })
+        axios.get(`${BaseUrl}api/article-subscribe?total=true&user_id=${result.data.id}`,{
+            headers: {Authorization: JSON.parse(account).token}
+          }).then(result => {
+          this.setState({totalSubscribe: result.data.total})
+        })
+        axios.get(`${BaseUrl}api/article?total=true&user_id=${result.data.id}`, {headers: {Authorization: JSON.parse(account).token}}).then(result => {
+          this.setState({totalArticle: result.data.total})
+        })
       }
     })
   }
@@ -75,102 +85,116 @@ class ShowProfileCMP extends React.Component {
       <React.Fragment>
       {
         this.state.found ?
-        <React.Fragment>
-        <div className="parallax-container default">
-          <div className="parallax">
-            <img src={this.state.users['profil-cover'] ? BaseUrl + 'api/usrfile/' + this.state.users.id + '/' + this.state.users['profil-cover']: Config.users.cover}/>
-          </div>
-          <div className="row">
-            <div className="col s12">
+        <ContextDATA.Consumer>
+        {
+          result => (
+            <React.Fragment>
+              <div className="parallax-container default">
+                <div className="parallax">
+                  <img src={this.state.users['profil-cover'] ? BaseUrl + 'api/usrfile/' + this.state.users.id + '/' + this.state.users['profil-cover']: Config.users.cover}/>
+                </div>
                 <div className="row">
                   <div className="col s12">
-                    <div className="center-align">
-                      <img style={{maxWidth: '100%', maxHeight: 167}} className="circle waves-effect waves-light" src={this.state.users.avatar.length == 0 ? Config.users.avatarDefault: `${BaseUrl}api/usrfile/${this.props.match.params.id}/${this.state.users.avatar}`}/>
-                    </div>
-                  </div>
-                </div>
-            </div>
-          </div>
-        </div>
-          <div className="row">
-            <div className="col s12 m8 offset-m2 white z-depth-1" style={{marginTop:'-100px'}}>
-              <div className="black-text">
-                <h5>{this.state.users.name}</h5>
-                <div className="divider"/>
-                <p className="black-text">{this.state.users.bio == null ? 'Bio is not created by its users': this.state.users.bio}</p>
-                <div className="divider"/>
-                <div className="row mt-10px">
-                  <div className="col s11">
-                  <ContextDATA.Consumer>
-                    {
-                      result => (
-                        <React.Fragment>
-                          <button
-                            disabled={result.users.id ? result.users.id !== this.state.users.id? false: true: true}
-                            className="btn waves-light waves-effect blue mt-10px"
-                            onClick={this.addSubscribe}
-                          >{this.state.statusSubscribe}</button>
-                          <ReportUser user_id={this.state.users.id} disabled={result.users.id ? result.users.id !== this.state.users.id? false: true: true}/>
-                        </React.Fragment>
-                      )
-                    }
-                  </ContextDATA.Consumer>
+                      <div className="row">
+                        <div className="col s12">
+                          <div className="center-align">
+                            <img style={{maxWidth: '100%', maxHeight: 167}} className="circle waves-effect waves-light" src={this.state.users.avatar.length == 0 ? Config.users.avatarDefault: `${BaseUrl}api/usrfile/${this.props.match.params.id}/${this.state.users.avatar}`}/>
+                          </div>
+                        </div>
+                      </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          {
-            this.state.users.id ? <Slider url={`${BaseUrl}api/article`} query={"popular=true&user_id=" + this.state.users.id}  />:false
-          }
-          <div className="row">
-            <div className="col s12">
-              <ul className="tabs">
-                <li className="tab col s6"><a className="active" href="#blog-tabs">Blog</a></li>
-                <li className="tab col s6"><a href="#information-blog">Information</a></li>
-              </ul>
-            </div>
-            <div id="blog-tabs" className="col s12">
-              <h5 style={{marginLeft:10}}>Post Blog</h5>
+              <div className="row">
+                <div className="col s12 m8 offset-m2 white z-depth-1" style={{marginTop:'-100px'}}>
+                  <div className="black-text">
+                    <h5>{this.state.users.name}</h5>
+                    <div className="divider"/>
+                    <p className="black-text">{this.state.users.bio == null ? 'Bio is not created by its users': this.state.users.bio}</p>
+                    <div className="divider"/>
+                    <div className="row mt-10px">
+                      <div className="col s11">
+                        <button
+                          disabled={result.users.id ? result.users.id !== this.state.users.id? false: true: true}
+                          className="btn waves-light waves-effect blue mt-10px"
+                          onClick={this.addSubscribe}
+                        >{this.state.statusSubscribe}</button>
+                        <ReportUser user_id={this.state.users.id} disabled={result.users.id ? result.users.id !== this.state.users.id? false: true: true}/>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col s12 m6 l3 offset-l3 waves-effect waves-dark">
+                  <div className={result.ui.navbar.bg + " darken-1 center white-text card-panel z-depth-0"}>
+                    <i className="material-icons" style={{fontSize: 50}}>notes</i>
+                    <h6 style={{marginTop: 0}}>Article</h6>
+                    <p><b>{this.state.totalArticle}</b></p>
+                  </div>
+                </div>
+                <div className="col s12 m6 l3 waves-effect waves-dark">
+                  <div className={result.ui.navbar.bg + " darken-1 lighten-1 center white-text card-panel z-depth-0"}>
+                    <i className="material-icons" style={{fontSize: 50}}>subscriptions</i>
+                    <h6 style={{marginTop: 0}}>Subscriber</h6>
+                    <p><b>{this.state.totalSubscribe}</b></p>
+                  </div>
+                </div>
+              </div>
               {
-                this.state.users.id ? <ArticleLoadCMP url={BaseUrl + 'api/article'} query={"users=" + this.state.users.id} id_next="next_article"/>:''
+                this.state.users.id ? <Slider url={`${BaseUrl}api/article`} query={"popular=true&user_id=" + this.state.users.id}  />:false
               }
-              {
-                this.state.finished ? '': <Loader/>
-              }
-            </div>
-            <div id="information-blog" className="col s12">
-              <ul className="collection">
-                <li className="collection-item avatar">
-                  <i className="material-icons circle red">badge</i>
-                  <span className="title">Name</span>
-                  <p>{this.state.users.name}</p>
-                </li>
-                <li className="collection-item avatar">
-                  <i className="material-icons circle red">account_circle</i>
-                  <span className="title">Gender</span>
-                  <p>{this.state.users.gender}</p>
-                </li>
-                <li className="collection-item avatar">
-                  <i className="material-icons circle red">calendar_today</i>
-                  <span className="title">Born</span>
-                  <p>{this.state.users.born}</p>
-                </li>
-                <li className="collection-item avatar">
-                  <i className="material-icons circle red">place</i>
-                  <span className="title">Location</span>
-                  <p>{this.state.users.location}</p>
-                </li>
-                <li className="collection-item avatar">
-                  <i className="material-icons circle red">description</i>
-                  <span className="title">Bio</span>
-                  <p>{this.state.users.bio == null ? 'Bio is not created by its users': this.state.users.bio}</p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </React.Fragment>
-        :''
+              <div className="row">
+                <div className="col s12">
+                  <ul className="tabs">
+                    <li className="tab col s6"><a className="active" href="#blog-tabs">Blog</a></li>
+                    <li className="tab col s6"><a href="#information-blog">Information</a></li>
+                  </ul>
+                </div>
+                <div id="blog-tabs" className="col s12">
+                  <h5 style={{marginLeft:10}}>Post Blog</h5>
+                  {
+                    this.state.users.id ? <ArticleLoadCMP url={BaseUrl + 'api/article'} query={"users=" + this.state.users.id} id_next="next_article"/>:''
+                  }
+                  {
+                    this.state.finished ? '': <Loader/>
+                  }
+                </div>
+                <div id="information-blog" className="col s12">
+                  <ul className="collection">
+                    <li className="collection-item avatar">
+                      <i className="material-icons circle red">badge</i>
+                      <span className="title">Name</span>
+                      <p>{this.state.users.name}</p>
+                    </li>
+                    <li className="collection-item avatar">
+                      <i className="material-icons circle red">account_circle</i>
+                      <span className="title">Gender</span>
+                      <p>{this.state.users.gender}</p>
+                    </li>
+                    <li className="collection-item avatar">
+                      <i className="material-icons circle red">calendar_today</i>
+                      <span className="title">Born</span>
+                      <p>{this.state.users.born}</p>
+                    </li>
+                    <li className="collection-item avatar">
+                      <i className="material-icons circle red">place</i>
+                      <span className="title">Location</span>
+                      <p>{this.state.users.location}</p>
+                    </li>
+                    <li className="collection-item avatar">
+                      <i className="material-icons circle red">description</i>
+                      <span className="title">Bio</span>
+                      <p>{this.state.users.bio == null ? 'Bio is not created by its users': this.state.users.bio}</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </React.Fragment>
+          )
+        }
+        </ContextDATA.Consumer>
+        :<div style={{height: 400}}/>
       }
       </React.Fragment>
     )

8ca4a95e4e4429fbbf14ae253dad068a99d52ad7
Ferdiansyah <ferdif9996@gmail.com>
2021-02-23 05:25:18 +0700

    Change image users avatarDefault Config.js

--- resources/js/Config.js
+++ resources/js/Config.js
@@ -8,7 +8,7 @@ const Config = {
 	},
 	users: {
 		cover: 'https://images.unsplash.com/photo-1606044466411-207a9a49711f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHw%3D&auto=format&fit=crop&w=600&q=60',
-		avatarDefault: 'https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80'
+		avatarDefault: 'https://images.unsplash.com/photo-1513956589380-bad6acb9b9d4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80'
 	},
 	sidebar: {
     	bg: '',
